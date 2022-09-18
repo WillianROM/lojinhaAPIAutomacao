cceed8e496704197ec2b86dc229650c1fab44c18
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-09-18 14:33:15 -0300

    Create UsuarioPojo.java

---
+++ src/test/java/pojo/UsuarioPojo.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class UsuarioPojo {
+    private String usuarioLogin;
+    private String usuarioSenha;
+
+    public String getUsuarioLogin() {
+        return usuarioLogin;
+    }
+
+    public void setUsuarioLogin(String usuarioLogin) {
+        this.usuarioLogin = usuarioLogin;
+    }
+
+    public String getUsuarioSenha() {
+        return usuarioSenha;
+    }
+
+    public void setUsuarioSenha(String usuarioSenha) {
+        this.usuarioSenha = usuarioSenha;
+    }
+}
