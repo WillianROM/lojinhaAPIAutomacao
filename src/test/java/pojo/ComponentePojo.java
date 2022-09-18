9c30ad2aaa4f5b268d0a45db1e0e4f7504618826
Willian Melo <109000079+WillianROM@users.noreply.github.com>
2022-09-18 14:33:04 -0300

    Create ComponentePojo.java

---
+++ src/test/java/pojo/ComponentePojo.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class ComponentePojo {
+    private String componenteNome;
+    private int componenteQuantidade;
+
+    public String getComponenteNome() {
+        return componenteNome;
+    }
+
+    public void setComponenteNome(String componenteNome) {
+        this.componenteNome = componenteNome;
+    }
+
+    public int getComponenteQuantidade() {
+        return componenteQuantidade;
+    }
+
+    public void setComponenteQuantidade(int componenteQuantidade) {
+        this.componenteQuantidade = componenteQuantidade;
+    }
+}
